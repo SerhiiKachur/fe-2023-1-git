02555884ae473f6b28a1048a4c558408fa107f5c
SerhiiKachur <kachurserega28@gmail.com>
2023-04-05 19:49:04 +0300

    Innitial commit for js let and const example

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let username = 10;
+console.log(username);
+username = 50;
+console.log(username);
+const username2 = 100;
+console.log(username2);
+username2 = 500;
+username = 15;
+console.log(username);
