9519f8945145484c4ebdd4009c30271052795fed
SerhiiKachur <kachurserega28@gmail.com>
2023-04-12 12:19:49 +0300

    Task commit

--- js/firsttask.js
+++ js/firsttask.js
@@ -1,6 +1,10 @@
 let userPrice = +prompt("Enter your price to see your discout (uah):");
-if (userPrice >= 1500) {
-  console.log(userPrice - userPrice * 0.1);
-} else if (userPrice >= 500) {
-  console.log(userPrice - userPrice * 0.05);
+if (userPrice > 1500) {
+  alert('Ви заплатите ' + (userPrice - userPrice * 0.1) + ' грн');
+} else if (userPrice > 500) {
+  alert('Ви заплатите ' + (userPrice - userPrice * 0.05) + ' грн');
+} else if (userPrice <= 500) {
+  alert('Ви заплатите ' + userPrice + ' грн');
+} else if (userPrice < 0) {
+  alert('Операція неможлива!')
 }
---
+++ js/index.js
@@ -0,0 +1,13 @@
+const getBiggestNumber = function (number1, number2){
+  if (
+    typeof number1 === 'number' &&
+    typeof number2 === 'number' &&
+    !isNaN(number1) &&
+    !isNaN(number2)
+  ) {
+    const bigNumber = number1 + number2;
+
+    return result;
+  }
+
+}
