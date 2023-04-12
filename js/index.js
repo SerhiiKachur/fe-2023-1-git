8d8faca200b2918500bb71cdd7d05df8291b9659
SerhiiKachur <kachurserega28@gmail.com>
2023-04-12 12:58:10 +0300

    Hometask commit

--- js/index.js
+++ js/index.js
@@ -1,13 +1,30 @@
-const getBiggestNumber = function (number1, number2){
+/**
+ * @returns {number}
+ * @param {number} number1
+ * @param {number} number2
+ */
+const getBiggestNumber = function (number1, number2) {
   if (
-    typeof number1 === 'number' &&
-    typeof number2 === 'number' &&
-    !isNaN(number1) &&
-    !isNaN(number2)
+    typeof number1 !== "number" ||
+    typeof number2 !== "number" ||
+    isNaN(number1 - number2)
   ) {
-    const bigNumber = number1 + number2;
-
-    return result;
+    return null;
   }
+  if (number1 > number2) {
+    return number1;
+  }
+  return number2;
+};
+getBiggestNumber();
 
-}
+const isNumberEven = function (num) {
+  if (typeof num !== "number" || isNaN(num)) {
+    return null;
+  }
+  if (num % 2 === 0) {
+    return true;
+  }
+  return false;
+};
+isNumberEven();
