fc0a003aa99bef6789c30b2d9476456dd6aaf294
SerhiiKachur <kachurserega28@gmail.com>
2023-04-28 20:35:36 +0300

    Task for class construktor

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ class Worker {
   }
 
   fullname() {
-    return `Hello! My name is ${this.name + " " + this.secondname}`;
+    return `${this.name} ${this.secondname}`;
   }
 
   isAdult() {
@@ -17,4 +17,21 @@ class Worker {
   }
 }
 
+
+class Order {
+  constructor(client, adress, status, product) {
+    this.client = client;
+    this.adress = adress;
+    this.status = status;
+    this.product = product;
+  }
+  print() {
+    return `This order goes to ${this.client.fullname()} who ordered ${
+      this.product
+    }.Delivery adress ${this.adress}, status is :${this.status}}`;
+  }
+}
+
 const worker1 = new Worker("Test", "Testenko", 20, "Five years");
+
+const order1 = new Order(worker1,'vyl. Heroiv 1','Delivered','pizza 4 cheese');
