26de1487b48b57caa642eb2fc35d8a442e5facf9
SerhiiKachur <kachurserega28@gmail.com>
2023-05-15 18:05:28 +0300

    Add empty string defense

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@
 
 const sectionContainer = document.getElementById("main-container");
 
-
 const tasksContainer = document.createElement("ol");
 sectionContainer.append(tasksContainer);
 tasksContainer.classList.add("main-container");
@@ -14,21 +13,24 @@ createBtn.classList.add("btn");
 
 function createBtnPush() {
   const taskFromPrompt = prompt("Discribe your task");
+  if(taskFromPrompt === ''){
+    alert('You need to write something');
+    return;
+  }
   const newLi = document.createElement("li");
   tasksContainer.append(newLi);
   newLi.textContent = taskFromPrompt;
-  newLi.classList.add('li-item');
-  const deleteBtn = document.createElement('button');
-  deleteBtn.textContent = 'x';
-  deleteBtn.classList.add('delbtn');
+  newLi.classList.add("li-item");
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "x";
+  deleteBtn.classList.add("delbtn");
   newLi.append(deleteBtn);
-  function deleteTask(){
+  function deleteTask() {
     tasksContainer.removeChild(newLi);
   }
-  deleteBtn.addEventListener('click',deleteTask);
+  deleteBtn.addEventListener("click", deleteTask);
 }
 
-createBtn.addEventListener('click', createBtnPush);
-
+createBtn.addEventListener("click", createBtnPush);
 
 tasksContainer.textContent = `Tap Green Button to create new task`;
