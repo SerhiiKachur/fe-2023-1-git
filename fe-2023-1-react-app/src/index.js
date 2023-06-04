7b1b1275cb2924163113c181330aac0fb1fb1620
SerhiiKachur <kachurserega28@gmail.com>
2023-06-04 13:40:35 +0300

    Add variants of props JSX

--- fe-2023-1-react-app/src/index.js
+++ fe-2023-1-react-app/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 
 class Header extends React.Component {
   render() {
+    console.log(this.props);
     // const h1 = React.createElement("h1", {}, "My Site");
 
     // const nav = React.createElement("nav", {}, "This is nav");
@@ -17,14 +18,27 @@ class Header extends React.Component {
     //   h1,
     //   nav
     // );
+    const { titleText } = this.props;
     return (
-      <header id="header" className="class">
+      <header id="header" className="class" title={titleText}>
         <h1>My Site</h1>
         <nav>this is nav</nav>
       </header>
     );
   }
 }
+
+class Greeting extends React.Component {
+  render() {
+    const { name } = this.props;
+    console.log(name);
+    return <p>Hello {name}!</p>;
+  }
+}
+// const greeting1 = React.createElement(Greeting, { name: "test" });
+
+const greeting1 = <Greeting name="Test"></Greeting>;
+// const greeting1 = <Greeting name="Test" />; якщо немає чілдренів(дітей (тексту)) теж саме що в строці зверху
 
 class Counter extends React.Component {
   constructor(props) {
@@ -87,7 +101,13 @@ const elem = React.createElement(Header);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // const elem = React.createElement('header',{id:"header", className:'class'},'children');
-const container = React.createElement(React.Fragment, {}, elem, counter1);
+const container = React.createElement(
+  React.Fragment,
+  {},
+  elem,
+  counter1,
+  greeting1
+);
 
 root.render(container);
 
