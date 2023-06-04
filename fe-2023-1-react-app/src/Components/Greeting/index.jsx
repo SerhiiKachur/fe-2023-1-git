bc2c479e8b508ff54a93240e98c1ffd4ff8c0624
SerhiiKachur <kachurserega28@gmail.com>
2023-06-04 15:41:12 +0300

    Add bacis css styles imports to react

--- fe-2023-1-react-app/src/Components/Greeting/index.jsx
+++ fe-2023-1-react-app/src/Components/Greeting/index.jsx
@@ -1,24 +1,27 @@
 import React from "react";
+import "./style.css";
 
-// class Greeting extends React.Component {
-//   render() {
-//     const { name } = this.props;
-//     // console.log(name);
-//     // 1. Умовний рендерінг з перевіркою якщо невказаний пропс(name);
-//     // if (!name) {
-//     // return <p>Hello Guest!</p>;
-//     // }
-//     // return <p>Hello {name}!</p>;
+class Greeting extends React.Component {
+  render() {
+    const { name } = this.props;
+    // console.log(name);
+    // 1. Умовний рендерінг з перевіркою якщо невказаний пропс(name);
+    // if (!name) {
+    // return <p>Hello Guest!</p>;
+    // }
+    // return <p>Hello {name}!</p>;
 
-//     // 2. Тернарна версія перевірки на пустий пропс:
-//     // const userName = !name ? 'Guest': name;
-//     // return <p>Hello {userName}!</p>;
+    // 2. Тернарна версія перевірки на пустий пропс:
+    // const userName = !name ? 'Guest': name;
+    // return <p>Hello {userName}!</p>;
 
-//     // 2.5 Тернарна версія перевірки без створення окремої змінної
-//     // одразу повертаємо
-//     return <p>Hello {!name ? "Guest" : name} </p>;
-//   }
-// }
+    // 2.5 Тернарна версія перевірки без створення окремої змінної
+    // одразу повертаємо
+    
+
+    return <p className="greetingText">Hello {!name ? "Guest" : name} </p>;
+  }
+}
 
 
 // 3. Через створеня додаткових функцій (змінних)
@@ -32,15 +35,15 @@ import React from "react";
 
 // 3.5 Через тернарку:
 
-function Greeting (props){
-  const {name }=props;
-  return !name ? <GreetGuest /> : <GreetUser userName={name}/>;
-}
+// function Greeting (props){
+//   const {name }=props;
+//   return !name ? <GreetGuest /> : <GreetUser userName={name}/>;
+// }
 
-function GreetUser(props) {
-  return <p>Hello {props.userName}!</p>
-}
+// function GreetUser(props) {
+//   return <p>Hello {props.userName}!</p>
+// }
 
-const GreetGuest = (props)=> <p>Hello Guest!</p>
+// const GreetGuest = (props)=> <p>Hello Guest!</p>
 
 export default Greeting;
