e3589e770c0e245174fa481dc1d68bff61c6e84c
SerhiiKachur <kachurserega28@gmail.com>
2023-06-06 20:50:31 +0300

    Add fix to start button

--- fe-2023-1-react-app/src/Components/StopWatch/index.jsx
+++ fe-2023-1-react-app/src/Components/StopWatch/index.jsx
@@ -6,29 +6,40 @@ class StopWatch extends React.Component {
     this.state = {
       currentTime: 0,
     };
+    this.intervalID = null;
   }
 
   start = () => {
     // const {currentTime} =this.state;
+    if (this.intervalID) {
+      return;
+    }
     this.intervalID = setInterval(() => {
       this.setState({
         currentTime: this.state.currentTime + 1,
       });
     }, 1000);
   };
+
+  stop = () => {
+    clearInterval(this.intervalID);
+    this.intervalID = null;
+
+  }
+
   componentDidMount() {
     console.log("componentDidMount");
     this.start();
   }
+  
   componentDidUpdate(prevProps, prevState) {
     console.log("componentDidUpdate");
   }
 
-  componentWillUnmount(){
-    console.log('componentWillUnmount');
-    clearInterval(this.intervalID);
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+    this.stop();
   }
-
 
   render() {
     const { currentTime } = this.state;
@@ -36,7 +47,7 @@ class StopWatch extends React.Component {
       <section>
         <p>{currentTime}</p>
         <button onClick={this.start}>Start</button>
-        <button>Stop</button>
+        <button onClick={this.stop}>Stop</button>
       </section>
     );
   }
